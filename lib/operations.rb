b53874ff5b57a286eb0ced63ad6a7e53f7633d4c
John Schmidley <tlg8n@outlook.com>
2019-09-13 13:34:11 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,18 @@
 def unsafe?(speed)
-
+  if speed > 60
+    return true
+  elsif speed < 40
+    return true
+  elsif speed < 40 or speed < 60
+    return false
+  end
 end
-
-
+ 
 
 def not_safe?(speed)
-	
+  
 end
+
 	
 
 
